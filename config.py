98e3272765ab4cb43cca68aeb536f2295f78382e
so-ras <soras.jahangiri@gmail.com>
2026-08-10 00:25:47 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+BOT_TOKEN = "8882672601:AAGJos3r1VJPYKiJuoPL9RW5kR3O92iezpA"
+OWNER_ID = 1391789851
+
+WARN_LIMIT = 3
+FLOOD_LIMIT = 5
+REWARD_ENABLED = True
+DEFAULT_LANG = "fa"
+
+SYMBOLS = {
+    "success": "[+] ",
+    "error": "[-] ",
+    "warning": "[!] ",
+    "info": "[*] ",
+    "ban": "✖ ",
+    "mute": "⊘ ",
+    "warn": "⚠ ",
+    "promote": "✦ ",
+    "demote": "⬇ ",
+    "lock": "☒ ",
+    "unlock": "☐ ",
+    "filter": "☢ ",
+    "stats": "≡ ",
+    "info": "ℹ ",
+}
